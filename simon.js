4e1dafccc46869c07ef8138640746cd7731fe276
Ujjval Pateliya <ujjvalpateliya@gmail.com>
2025-02-14 21:40:01 +0530

    New Update Gmae

--- simon.js
+++ simon.js
@@ -1,89 +1,78 @@
-// Hii I'm Pawan !!
+const colors = ["red", "green", "orange", "blue"];
+let gameSequence = [];
+let userSequence = [];
+let level = 0;
+let started = false;
+let highScore = 0;
 
-let gameseq=[];
-let userseq=[];
-let started=false;
-let randcol=["red","green","orange","blue"];
+document.addEventListener("keydown", startGame);
+document.getElementById("startGame").addEventListener("click", startGame);
 
-let level=0;
-let h3=document.querySelector(".score");
-document.addEventListener("keypress",()=>{
-   if(started==false){
-    console.log("started!!");
-    started=true;
-    levelup();
-   
-   }
-})
-
-
-      function checkbtn(idx){
-        
-        
-        
-            if(gameseq[idx]==userseq[idx]){
-               if(gameseq.length==userseq.length){
-              setTimeout(levelup,1000);
-            }
-         
-         }
-         else{
-            h3.innerHTML=`Game Over !! <br>Your Score is ${level} ... <br> Press any key to start  😥`;
-            document.querySelector("body").style.backgroundColor="red";
-            setTimeout(()=>{
-               document.querySelector("body").style.backgroundColor="white";
-               reset();
-            },250);
-         }
-      }
-
-function reset(){
-   gameseq=[];
-   userseq=[];
-   started=false;
-   level=0;
-
+function startGame() {
+    if (!started) {
+        started = true;
+        level = 0;
+        gameSequence = [];
+        userSequence = [];
+        document.querySelector(".score").textContent = "Level " + level;
+        document.querySelector(".overlay").classList.add("hidden");
+        nextSequence();
+    }
 }
 
- function btnpress (){
-    
-      let btn= this;
+function nextSequence() {
+    userSequence = [];
+    level++;
+    document.querySelector(".score").textContent = "Level " + level;
 
-      btnflash(btn);
-   //   userseq.push();
-   let user= btn.getAttribute("id");
- 
-   userseq.push(user);
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    gameSequence.push(randomColor);
 
+    setTimeout(() => {
+        playFlash(randomColor);
+    }, 500);
+}
 
-   checkbtn(userseq.length-1);
+function playFlash(color) {
+    let btn = document.getElementById(color);
+    btn.classList.add("flash");
+    playSound(color);
+    setTimeout(() => {
+        btn.classList.remove("flash");
+    }, 300);
+}
 
-      }
+function playSound(color) {
+    let audio = new Audio(`sounds/${color}.mp3`);
+    audio.play();
+}
 
-   let allbtn=document.querySelectorAll(".btn");
-   for(btn of allbtn){
-      btn.addEventListener("click",btnpress);
+document.querySelectorAll(".btn").forEach(btn => {
+    btn.addEventListener("click", function () {
+        let chosenColor = this.id;
+        userSequence.push(chosenColor);
+        playFlash(chosenColor);
+        playSound(chosenColor);
+        checkAnswer(userSequence.length - 1);
+    });
+});
 
-   };
-
-  
-
-   btnflash=(btn)=>{
-      btn.classList.add("flash");
-      setTimeout(()=>{
-         btn.classList.remove("flash");
-      }, 250);
-   }
-
-levelup = () =>{
-   userseq=[];
-   level++;
-   h3.innerText=`level ${level}`;
-   let index=Math.floor(Math.random()*3);
-   let rn=randcol[index];
-   gameseq.push(rn);
-
-   let ranbtn=document.querySelector(`.${rn}`)
-     btnflash(ranbtn);
-     console.log(gameseq);
+function checkAnswer(currentLevel) {
+    if (userSequence[currentLevel] === gameSequence[currentLevel]) {
+        if (userSequence.length === gameSequence.length) {
+            setTimeout(nextSequence, 1000);
+        }
+    } else {
+        playSound("wrong");
+        document.body.classList.add("game-over");
+        setTimeout(() => document.body.classList.remove("game-over"), 500);
+        
+        document.querySelector(".score").textContent = `Game Over! High Score: ${Math.max(level - 1, highScore)}. Press Any Key to Restart.`;
+        document.querySelector(".overlay").classList.remove("hidden");
+        
+        highScore = Math.max(level - 1, highScore);
+        document.querySelector(".high-score").textContent = "High Score: " + highScore;
+        
+        started = false;
+    }
 }
